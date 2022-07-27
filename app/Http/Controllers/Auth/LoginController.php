9956718c1bda575a456873c8c4cfbb5bf1dd615d
arifnugrahaa <arifnugraha1243@gmail.com>
2022-07-27 21:34:58 +0700

    [Preventing Login for New User]

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class LoginController extends Controller
 {
@@ -35,5 +37,26 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function validateLogin(Request $request)
+    {
+        $this->validate($request, [
+            $this->username() => [
+                'required','string',
+                Rule::exists('users')->where(function ($query){
+                    $query->where('active', true);
+                })
+            ], 
+            'password' => 'required|string',
+        ], $this->validationError());
+    }
+
+    // validasi
+    public function validationError()
+    {
+        return [
+            $this->username() . '.exists' => 'Email belum terdaftar, silahkan aktivasi terlebih dahulu'
+        ];
     }
 }
